7379c83259b3cdf7ebd34a03ba33b30dceb3c2fa
rzkprtmptr <kikirrizki@gmail.com>
2019-05-12 12:01:07 +0700

    1

---
+++ app/src/main/java/com/example/biodatanegaraasiatenggara/Activity/TabMapNegara.java
@@ -0,0 +1,30 @@
+package com.example.biodatanegaraasiatenggara.Activity;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.biodatanegaraasiatenggara.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TabMapNegara extends Fragment {
+
+
+    public TabMapNegara() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_map_negara, container, false);
+    }
+
+}
